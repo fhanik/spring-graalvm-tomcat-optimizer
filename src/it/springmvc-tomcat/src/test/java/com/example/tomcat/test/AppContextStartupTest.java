51c2c6644dc2f7ac713594f6617696c3d1706cf2
Filip Hanik <fhanik@pivotal.io>
2020-07-20 10:01:06 -0700

    Tomcat Graal Example

---
+++ src/it/springmvc-tomcat/src/test/java/com/example/tomcat/test/AppContextStartupTest.java
@@ -0,0 +1,14 @@
+package com.example.tomcat.test;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppContextStartupTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
